cd8965877853743023eead62c1cd77385890c6c4
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-19 17:51:42 -0300

    Future Time

--- futureTime.js
+++ futureTime.js
@@ -2,7 +2,31 @@
 
 const prompt = require("prompt-sync")();
 
-let timeCurrent = parseInt(prompt("current time: "));
-let numberWhole = parseInt(prompt("whole number: "));
+let currentHour;
+let number;
 
+do {
+  currentHour = parseInt(prompt("Enter the hour: "));
 
+  if (isNaN(currentHour)) {
+    console.log("Error: Enter a valid number!");
+  }
+  if (currentHour < 0 || currentHour > 23) {
+    console.log("Error: Enter correct time!");
+  }
+} while (isNaN(currentHour) || currentHour < 0 || currentHour > 23);
+
+do {
+  number = parseInt(prompt("Enter a number: "));
+  if (isNaN(number)) {
+    console.log("Error: Enter a valid number!");
+  }
+  if (number < 0) {
+    console.log("Error: negative numbers are not allowed!");
+  }
+} while (isNaN(number) || number < 0);
+
+const horaMarcadaReloj = (currentHour + number) % 24;
+console.log(
+  "In " + number + " hour, " + "the clock will mark the " + horaMarcadaReloj
+);

640bc4731a006adade6bcbe2ebfc58e35bdd4bb7
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-23 15:00:36 -0300

    numero mayor

---
+++ numeroMayor.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+let number;
+let ordenumero = [];
+do {
+  number = prompt("Enter a number: ");
+
+  if (isNaN(number)) {
+    console.log("Error: Enter a valid number!");
+  }
+} while (isNaN(number));
+
+for (let i = 0; i < number; i++) {
+  const num = Number(prompt("Enter a number: "));
+  ordenumero.push(num);
+}
+console.log("el numero es " + Math.max( ...ordenumero));

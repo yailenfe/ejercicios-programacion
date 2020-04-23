f0f73a32a9fe6daec4c218460dc46371bc2c465e
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-23 08:15:08 -0300

    dibujo con asterisos Dos

---
+++ dibujoAsteriscoDos.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+let heithg;
+let astericos = "*";
+
+do {
+  heithg = prompt("Enter a number: ");
+
+  if (isNaN(heithg)) {
+    console.log("Error: Enter a valid number!");
+  }
+} while (isNaN(heithg));
+
+for (let i = 0; i < heithg; i++) {
+  console.log(astericos);
+  astericos += "*";
+}

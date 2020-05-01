9517a0914503238b83a6da8f4a87a00c7ed8cea4
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-30 23:30:06 -0300

    editando number Primo

--- numeroPrimo.js
+++ numeroPrimo.js
@@ -1,24 +1,22 @@
 const prompt = require("prompt-sync")();
 let number;
-let isPrime=true
+let isPrime = true;
 
 do {
     number = prompt("Enter a number: ");
     if (isNaN(number)) {
-        console.log("numeber invalid");
+        console.log("Number invalid!");
     }
 } while (isNaN(number));
 
 for (i = 2; i < number; i++) {
-    if(number%i===0){
-    
-        isPrime=false
-        
+    if (number % i === 0) {
+        isPrime = false;
     }
 }
 
-if(isPrime===true){
-    console.log(`${number} es prime`)
-}else{
-    console.log(`${number} is compound`)
+if (isPrime === true) {
+    console.log(`${number} is prime`);
+} else {
+    console.log(`${number} is compound`);
 }

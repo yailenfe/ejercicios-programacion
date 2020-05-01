901e6c7815a4f594426723805a1f0f12856c0b95
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-01 00:15:26 -0300

    numero primo2

---
+++ numeroPrimo2.js
@@ -0,0 +1,26 @@
+const prompt = require("prompt-sync")();
+let number;
+let cantPrimes = 0;
+
+do {
+    number = prompt("Enter a number: ");
+    if (isNaN(number)) {
+        console.log("Number invalid!");
+    }
+} while (isNaN(number));
+
+for (let i = 2; i < number; i++) {
+  
+    let esPrime = true;
+    
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          esPrime = false;
+        }
+    }
+    
+    if (esPrime) {
+      console.log(i);
+    
+    }
+  }

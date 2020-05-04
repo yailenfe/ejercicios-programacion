b2e6dd7135f39280e592824599d8af1e7dab272a
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-04 16:06:28 -0300

    resolve PR comennts

--- condicionales/numeroParOImpar.js
+++ condicionales/numeroParOImpar.js
@@ -1,13 +1,13 @@
 // Escriba un programa que pida primero un número par y luego un número impar (positivos o negativos). En caso de que uno o los dos valores no sea correcto, se mostrará un único aviso.
 const prompt = require("prompt-sync")();
 
-let numberEven;
-let numberOdd;
+let evenNumber;
+let oddNumber;
 
-numberEven = Number(prompt("Enter a Even number: "));
-numberOdd = Number(prompt("Enter a Odd number: "));
+evenNumber = Number(prompt("Enter a even number: "));
+oddNumber = Number(prompt("Enter a odd number: "));
 
-if (numberEven % 2 === 0 && numberOdd % 2 != 0) {
+if (evenNumber % 2 === 0 && oddNumber % 2 != 0) {
       console.log("Thank you for your cooperation!");
 } else {
       console.log(

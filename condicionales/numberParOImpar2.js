92abb59c0a02b14b2165479f7c926baa4cf0354e
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-04 13:27:10 -0300

    conditionales 2

---
+++ condicionales/numberParOImpar2.js
@@ -0,0 +1,26 @@
+// Escriba un programa que pida primero un número par (positivo o negativo) y si el valor no es correcto, muestre un aviso. Si el valor es correcto, pedirá un número impar (positivo o negativo) y si el valor no es correcto, mostrará un aviso.
+const prompt = require("prompt-sync")();
+
+let numberEven;
+let numberOdd;
+
+do {
+      numberEven = parseInt(prompt("Enter a number Pair: "));
+
+      if (numberEven % 2 != 0) {
+            console.log(`Error: not write number pair`);
+            console.log(` Please run the program again to try again.`);
+      }
+} while (numberEven % 2 != 0);
+do {
+      numberOdd = parseInt(prompt(`Enter a number Odd:`));
+
+      if (numberOdd % 2 === 0) {
+            console.log(`Error: write a number Odd`);
+            console.log(` Please run the program again to try again.`);
+      }
+} while (numberOdd % 2 === 0);
+
+if (numberEven % 2 === 0 && numberOdd % 2 != 0) {
+      console.log(`Thank you for your cooperation!`);
+}

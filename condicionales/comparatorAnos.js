fd88306ad253277aab097ee2d6d7a7cb4679fdbe
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-05 11:04:01 -0300

    resolve PR comments

--- condicionales/comparatorAnos.js
+++ condicionales/comparatorAnos.js
@@ -1,17 +1,19 @@
-// Escriba un programa que pida el año actual y un
+// Escriba un programa que pida el año actual y un año cualquiera y que escriba cuántos años han pasado desde ese año o cuántos años faltan para llegar a ese año
 const prompt = require("prompt-sync")();
 
-const actulYear = Number(prompt("Enter the actul year: "));
-const secundYear = Number(prompt("Enter the second year: "));
+const actualYear = Number(prompt("Enter the actual year: "));
+const secondYear = Number(prompt("Enter the second year: "));
 
-let difenciaYear = Math.abs(actulYear - secundYear);
+let differenceYear = Math.abs(actualYear - secondYear);
 
-if (actulYear < secundYear) {
+if (actualYear < secondYear) {
 	console.log(
-		`To get to the year ${secundYear} missing ${difenciaYear} yaer`
+		`To get to the year ${secondYear} missing ${differenceYear} yaer`
 	);
-} else if (actulYear > secundYear) {
-	console.log(`since the year ${secundYear} has passed ${difenciaYear} year`);
-} else if (actulYear === secundYear) {
+} else if (actualYear > secondYear) {
+	console.log(
+		`since the year ${secondYear} has passed ${differenceYear} year`
+	);
+} else if (actualYear === secondYear) {
 	console.log("They are the same year");
 }

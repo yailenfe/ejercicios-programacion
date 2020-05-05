abdd3978cf8bf84907e35585d10c073cfd5d7a30
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-04 22:45:55 -0300

    resolve comments

--- condicionales/numberParOImparA2.js
+++ condicionales/numberParOImparA2.js
@@ -5,14 +5,14 @@ let oddNumber;
 
 evenNumber = Number(prompt("Enter a even number: "));
 if (evenNumber % 2 !== 0) {
-	console.log(`You have not written an even number.`);
-	console.log(`Please run the program again to try again.`);
-}
-
-oddNumber = Number(prompt("Enter a odd number: "));
-if (oddNumber % 2 === 0) {
-	console.log(`You have not written an odd number.`);
-	console.log(`Please run the program again to try again.`);
+	console.log("You have not written an even number.");
+	console.log("Please run the program again to try again.");
+} else {
+	oddNumber = Number(prompt("Enter a odd number: "));
+	if (oddNumber % 2 === 0) {
+		console.log("You have not written an odd number.");
+		console.log("Please run the program again to try again.");
+	}
 }
 
 if (evenNumber % 2 === 0 && oddNumber % 2 !== 0) {

6ec5a4480c24568614f2d09ee4087a81bb7f027c
Yailen Fajardo Egues <57881396+yailenfe@users.noreply.github.com>
2020-05-04 22:53:52 -0300

    condictional-A-3
    * condictional-A-3
    
    * resolve comments
    
    * resolve comments
    
    * resolve comments

---
+++ condicionales/numeroParoImparA3.js
@@ -0,0 +1,22 @@
+// Escriba un programa que pida primero un número par y a continuación un numero impar (positivos o negativos). En caso de que uno o los dos valores no sean correctos, se mostrarán uno o dos avisos.
+const prompt = require("prompt-sync")();
+
+let evenNumber;
+let oddNumber;
+
+evenNumber = Number(prompt("Enter an even number: "));
+oddNumber = Number(prompt("Enter an odd number: "));
+
+if (evenNumber % 2 !== 0) {
+	console.log("You have not written an even number");
+}
+
+if (oddNumber % 2 === 0) {
+	console.log("You have not written an odd number");
+}
+
+if (evenNumber % 2 === 0 && oddNumber % 2 !== 0) {
+	console.log("Thank you for your cooperation");
+} else {
+	console.log("Run a new program to try");
+}

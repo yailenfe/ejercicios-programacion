a9f55ea1bf354c20da7e25d5440d7f2927ea638d
Yailen Fajardo Egues <57881396+yailenfe@users.noreply.github.com>
2020-05-06 15:21:47 -0300

    conditional-2-4 (#11)
    * conditional-2-4
    
    * resolve PR commnets
    
    * resolve PR comments

---
+++ condicionales/comparatotMultiplos2-4.js
@@ -0,0 +1,19 @@
+// Escriba un programa que pida dos números enteros y que escriba si el mayor es múltiplo del menor.
+const prompt = require("prompt-sync")();
+
+const firstNumber = Number(prompt("Enter a first number: "));
+const secondNumber = Number(prompt("Enter a  second number: "));
+
+if (firstNumber > secondNumber) {
+	isMultiplo(firstNumber, secondNumber);
+} else {
+	isMultiplo(secondNumber, firstNumber);
+}
+
+function isMultiplo(numero1, number2) {
+	if (numero1 % number2 === 0) {
+		console.log(`${numero1} is multiple of ${number2}`);
+	} else {
+		console.log(`${numero1} is not multiple of ${number2}`);
+	}
+}

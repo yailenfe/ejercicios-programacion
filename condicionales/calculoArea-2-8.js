1ce6010026b609441c4c68f55922875194e7c956
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-07 16:11:45 -0300

    resolve PR comments

--- condicionales/calculoArea-2-8.js
+++ condicionales/calculoArea-2-8.js
@@ -3,8 +3,8 @@
 // Se recuerda que el área de un triángulo es base por altura dividido por 2 y que el área de un círculo es Pi (aproximadamente 3,141592) por el radio al cuadrado
 const prompt = require("prompt-sync")();
 console.log(`Choose a geometric figure: 
-a)Triangulo
-b)Circuferencia`);
+a)Triangle
+b)Circumference`);
 
 const figureChoosen = prompt(
 	"Which figure do you want to calculate (Write T or C)?"
@@ -20,6 +20,6 @@ if (figureChoosen.toLowerCase() === "t") {
 } else if (figureChoosen.toLowerCase() === "c") {
 	const radius = Number(prompt("Write the radius:"));
 	const PI = 3.141592;
-	const areaCircle = PI * (radius * radius);
-	console.log(`A circle of radius ${radius}it has an area of ${areaCircle}`);
+	const areaCircumference = PI * (radius * radius);
+	console.log(`A circle of radius ${radius}it has an area of ${areaCircumference}`);
 }

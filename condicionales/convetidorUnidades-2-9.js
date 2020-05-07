7d902da19beb812aa39f3a8043c2b6b1bf8168b2
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-07 15:11:29 -0300

    conditional-2-9

---
+++ condicionales/convetidorUnidades-2-9.js
@@ -0,0 +1,16 @@
+// Escriba un programa que pida una distancia en centímetros y que escriba esa distancia en kilómetros, metros y centímetros (escribiendo todas las unidades).
+const prompt = require("prompt-sync")();
+const number = prompt("Enter a  number centimetres : ");
+let kilometres = Math.floor(number / 100000);
+let restKilometres = number % 100000;
+let meter = Math.floor(restKilometres / 100);
+let restMeter = restKm % 100;
+let centimeter = restMeter;
+
+if (number <= 0) {
+	console.log("Write a distance greater than zero. ");
+} else {
+	console.log(
+		`${number}centimetres is ${kilometres} km ${meter} m ${centimeter} cm.`
+	);
+}

41bd9231741c954af8d96d34134ef0fd1572bb30
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-07 09:50:44 -0300

    resolve PR comments

--- condicionales/comparatotAnos-2-6.js
+++ condicionales/comparatotAnos-2-6.js
@@ -1,8 +1,8 @@
 // Escriba un programa que pida un año y que escriba si es bisiesto o no.Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100 no lo son, aunque los múltiplos de 400 sí.
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')();
 let year;
 
-year = prompt("Write a year and I will tell you if it is leap: ");
+year = Number(prompt('Write a year and I will tell you if it is leap: '));
 if (year % 400 === 0) {
 	console.log(
 		`The year ${year} is a leap year because it is a multiple of 400.`
@@ -12,8 +12,9 @@ if (year % 400 === 0) {
 		`The year ${year} is not a leap year because it is a multiple of 100 without being a multiple of 400`
 	);
 } else if (year % 4 === 0) {
-	console.log(`
-   The year ${year} is a leap year because it is a multiple of 4 without being a multiple of 100.`);
+	console.log(
+		`The year ${year} is a leap year because it is a multiple of 4 without being a multiple of 100.`
+	);
 } else {
 	console.log(`
     The year ${year} is not a leap year.`);

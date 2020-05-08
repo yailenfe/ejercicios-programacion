4a163484651884b71fbd48f47ad08d098aa775fc
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-05-07 22:22:50 -0300

    resolve PR comments

--- condicionales/minijuego-2.js
+++ condicionales/minijuego-2.js
@@ -1,23 +1,27 @@
+const prompt = require("prompt-sync")();
+const namePlayerA = prompt("Write you name: ");
+const namePlayerB = prompt("Write you name: ");
+
 const playerA1 = getRandomDice();
 const playerA2 = getRandomDice();
-const PlayerB1 = getRandomDice();
+const playerB1 = getRandomDice();
 const playerB2 = getRandomDice();
 
 const sumPlayerA = playerA1 + playerA2;
-const sumPlayerB = PlayerB1 + playerB2;
+const sumPlayerB = playerB1 + playerB2;
 
-console.log(`Carmen has taken ${playerA1} y ${playerA2}`);
-console.log(`David has taken ${PlayerB1} y ${playerB2}`);
+console.log(`${namePlayerA} has taken ${playerA1} and ${playerA2}`);
+console.log(`${namePlayerB} has taken ${playerB1} and ${playerB2}`);
 
 if (sumPlayerA > sumPlayerB) {
-	console.log("Has won player A");
+	console.log(`Has won ${namePlayerA}`);
 } else if (sumPlayerA < sumPlayerB) {
-	console.log("Has won player B");
+	console.log(`Has won ${namePlayerB}`);
 } else {
-	if (Math.max(playerA1, playerA2) > Math.max(PlayerB1, playerB2)) {
-		console.log("Has won player A");
-	} else if (Math.max(playerA1, playerA2) < Math.max(PlayerB1, playerB2)) {
-		console.log("Has won player B");
+	if (Math.max(playerA1, playerA2) > Math.max(playerB1, playerB2)) {
+		console.log(`Has won ${namePlayerA}`);
+	} else if (Math.max(playerA1, playerA2) < Math.max(playerB1, playerB2)) {
+		console.log(`Has won ${namePlayerB}`);
 	} else {
 		console.log("Have tied");
 	}

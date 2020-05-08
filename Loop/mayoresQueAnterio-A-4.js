5e0d49bf74caccdbfb8805c91211237d08db240c
Yailen Fajardo Egues <57881396+yailenfe@users.noreply.github.com>
2020-05-08 18:07:39 -0300

    loop-1-4 (#28)
    * loop-1-4
    
    * resolve PR comments
    
    * resolve PR comments

---
+++ Loop/mayoresQueAnterio-A-4.js
@@ -0,0 +1,25 @@
+// Escriba un programa que pregunte cuántos números se van a introducir, pida esos números, y muestre un mensaje cada vez que un número no sea mayor que el anterior
+const prompt = require("prompt-sync")();
+const amountOfValues = Number(
+	prompt("How many values ​​are you going to enter?: ")
+);
+
+if (amountOfValues < 0) {
+	console.log("Impossible!");
+} else {
+	let anterior;
+	for (i = 0; i < amountOfValues; i++) {
+		if (i === 0) {
+			anterior = Number(prompt("Write a number: "));
+		} else {
+			let valor = Number(
+				prompt(`Write a number greater than ${anterior}: `)
+			);
+			if (valor <= anterior) {
+				console.log(`!${valor} not is greater than ${anterior}`);
+			}
+			anterior = valor;
+		}
+	}
+	console.log("Thank you for your cooperation");
+}

d73aa9808351963685e4d6260973d8fa905b475d
Yailen Fajardo Egues <57881396+yailenfe@users.noreply.github.com>
2020-05-08 12:07:03 -0300

    loop-1-1 (#25)
    * loop-1-1
    
    * resolve PR comments

---
+++ Loop/paresOImpares-A-1.js
@@ -0,0 +1,20 @@
+// Escriba un programa que pida dos números enteros y escriba qué números son pares y cuáles impares desde el primero hasta el segundo.
+const prompt = require("prompt-sync")();
+const firtsNumber = Number(prompt("Write an integer:"));
+const secondNumber = Number(
+	prompt(`Write an integer greater than or equal to ${firtsNumber}:`)
+);
+
+if (secondNumber < firtsNumber) {
+	console.log(
+		`I have asked you for an integer greater than or equal to ${firtsNumber}`
+	);
+} else {
+	for (let i = firtsNumber; i <= secondNumber; i++) {
+		if (i % 2 == 0) {
+			console.log(`The number ${i} is even`);
+		} else {
+			console.log(`The number ${i} is odd`);
+		}
+	}
+}
